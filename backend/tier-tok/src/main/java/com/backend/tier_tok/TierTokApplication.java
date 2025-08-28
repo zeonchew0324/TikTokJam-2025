dace5b38fc0f7be6a85fc0a05a7bf93e00d7d2e0
Choy050823 <choymh23@gmail.com>
2025-08-28 11:36:06 +0800

    added entities for database

---
+++ backend/tier-tok/src/main/java/com/backend/tier_tok/TierTokApplication.java
@@ -0,0 +1,13 @@
+package com.backend.tier_tok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TierTokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TierTokApplication.class, args);
+	}
+
+}
---
+++ backend/tier-tok/src/main/java/com/backend/tier_tok/model/entity/User.java
@@ -0,0 +1,13 @@
+package com.backend.tier_tok.model.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+}
